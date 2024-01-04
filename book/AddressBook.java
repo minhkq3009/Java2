e291e561ac57939958d08d4e12160f47c11b801b
minhkq3009 <55178535+minhkq3009@users.noreply.github.com>
2024-01-04 10:28:46 +0700

    Update AddressBook.java

--- book/AddressBook.java
+++ book/AddressBook.java
@@ -1,12 +1,14 @@
 package book;
 
+import book.Contact;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class AddressBook {
     private final Map<String, Contact> addressBook = new HashMap<>();
-    private final Scanner scanner = new Scanner(System.in);
+    final Scanner scanner = new Scanner(System.in);
 
     public void addContact() {
         System.out.print("Enter name: ");
@@ -46,40 +48,5 @@ public class AddressBook {
 
     public void closeScanner() {
         scanner.close();
-    }
-
-    public static void main(String[] args) {
-        AddressBook addressBook = new AddressBook();
-        int choice;
-
-        do {
-            System.out.println("Address Book Menu:");
-            System.out.println("1. Add new contact");
-            System.out.println("2. Find a contact by name");
-            System.out.println("3. Display contacts");
-            System.out.println("4. Exit");
-            System.out.print("Enter your choice: ");
-            choice = addressBook.scanner.nextInt();
-            addressBook.scanner.nextLine(); // Consume the newline character
-
-            switch (choice) {
-                case 1:
-                    addressBook.addContact();
-                    break;
-                case 2:
-                    addressBook.findContactByName();
-                    break;
-                case 3:
-                    addressBook.displayContacts();
-                    break;
-                case 4:
-                    System.out.println("Exiting the Address Book. Goodbye!");
-                    break;
-                default:
-                    System.out.println("Invalid choice. Please enter a valid option.");
-            }
-        } while (choice != 4);
-
-        addressBook.closeScanner();
     }
 }
